be246e98ef932129bae80d99bf661322d9e93b27
ssenyonjo <gssenyonjo@gmail.com>
2020-08-13 21:13:33 +0300

    drew column chart using svg

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import './Dashboard.css'
 
 function randomDataset(count) {
     const dataset = []
-    for(let i = 0; i < count; i++) {
+    for (let i = 0; i < count; i++) {
         dataset.push(Math.floor(Math.random() * 31))
     }
     console.log(dataset)
@@ -13,30 +13,36 @@ function randomDataset(count) {
 }
 
 export const Dashboard = props => {
-    const dataset = randomDataset(5)
+    const dataset = randomDataset(10)
 
     const svg = d3.select("div").append("svg")
     const w = 500
     const h = 100
+    const barPadding = 5
     svg.attr("width", w)
         .attr("height", h)
 
-    const circles = svg.selectAll('circle')
+    svg.selectAll("rect")
         .data(dataset)
         .enter()
-        .append('circle')
-        .attr("fill", "yellow")
-        .attr("stroke", "orange")
-        .attr("stroke-width", d => d / 2)
+        .append("rect")
+        .attr("x", (d, i) => i * (w / dataset.length))
+        .attr("y", d => h - 2 * d)
+        .attr("width", w / dataset.length - barPadding)
+        .attr("height", d => 2 * d)
+        .attr("fill", d => `rgb(0, ${Math.round(10 * d)}, 0)`)
 
 
-    circles.attr("cx", (d, i) => {
-        return (i * 50) + 25
-    })
-        .attr("cy", h / 2)
-        .attr("r", d => d)
-
-
+    svg.selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .text(d => d)
+        .attr("x", (d, i) => i * w / dataset.length + (w / dataset.length - barPadding) / 2)
+        .attr("y", d => h - 7)
+        .attr("stroke", "white")
+        .attr("fill", "white")
+        .attr("text-anchor", "middle")
     return (
         <>
             <p>
