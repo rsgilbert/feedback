c83e397bfb4e4f845c1f06a62ed8ceb22ffcb681
ssenyonjo <gssenyonjo@gmail.com>
2020-08-14 16:31:15 +0300

    used various scales. scaleLinear, scaleSqrt, scaleTime. Also formated time using d3.timeFormat

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -13,36 +13,114 @@ function randomDataset(count) {
 }
 
 export const Dashboard = props => {
-    const dataset = randomDataset(10)
+    var dataset = [
+        [5, 10],  [200, 20], [400, 100],
+    [5, 20], [4, 90], [250, 50], [100, 33], [330, 95],
+    [410, 12], [475, 44], [25, 67], [85, 21], [220, 88], [50, 50]
+    ];
+    
+    var dates = [
+        {
+            timestamp: 1597410599034,
+            sales: 100
+        },
+        {
+            timestamp: 1597410591034,
+            sales: 500
+        },
+        {
+            timestamp: 1597410539034,
+            sales: 450
+        },
+        {
+            timestamp: 1597410559034,
+            sales: 200
+        },
+        {
+            timestamp: 1597410509034,
+            sales: 105
+        },
+    ]
+
+    const dateDataset = dates.map(d => ({
+        date: new Date(d.timestamp),
+        sales: d.sales
+    }))
+
+    const formatTime = d3.timeFormat("%b %e")
 
     const svg = d3.select("div").append("svg")
     const w = 500
-    const h = 100
-    const barPadding = 5
-    svg.attr("width", w)
-        .attr("height", h)
+    const h = 200
+    const padding = 20
 
-    svg.selectAll("rect")
-        .data(dataset)
+    const tScale = d3.scaleTime()
+        .domain([
+            d3.min(dateDataset, d => d.date),
+            d3.max(dateDataset, d => d.date)
+        ])
+        .range([padding, w + padding])
+
+
+    const salesScale = d3.scaleLinear()
+        .domain([0, d3.max(dateDataset, d => d.sales)])
+        .range([padding, h - padding])
+
+    // const xScale = d3.scaleLinear()
+    //     .domain([0, d3.max(dataset, d => d[0])])
+    //     .range([padding, w - padding])
+  
+
+    // const yScale = d3.scaleLinear()
+    //     .domain([0, d3.max(dataset, d => d[1])])
+    //     .range([h + padding, padding])
+
+    // const areaScale = d3.scaleSqrt()
+    //     .domain([0, d3.max(dataset, d=> d[1])])
+    //     .range([5, 15])
+    
+
+
+    svg.attr("width", w + 100)
+        .attr("height", h + 100)
+
+    // svg.selectAll("circle")
+    //     .data(dataset)
+    //     .enter()
+    //     .append("circle")
+    //     .attr("cx", d => xScale(d[0]))
+    //     .attr("cy", d => yScale(d[1]))
+    //     .attr("r", 5)
+    //     .attr("r", d => areaScale(d[1]))
+    //     // .attr("r", d => Math.sqrt(( 5 * d[1])))
+    //     .attr("fill", "red")
+
+    // svg.selectAll("text")
+    //     .data(dataset)
+    //     .enter()
+    //     .append("text")
+    //     .text(d => "(" + d[0] + ", " + d[1] + ")")
+    //     .attr("x", d => xScale(d[0]))
+    //     .attr("y", d => yScale(d[1]))
+    //     .attr("font-size", 14)
+
+    /* set scale for dates */
+    svg.selectAll("circle")
+        .data(dateDataset)
         .enter()
-        .append("rect")
-        .attr("x", (d, i) => i * (w / dataset.length))
-        .attr("y", d => h - 2 * d)
-        .attr("width", w / dataset.length - barPadding)
-        .attr("height", d => 2 * d)
-        .attr("fill", d => `rgb(0, ${Math.round(10 * d)}, 0)`)
-
+        .append("circle")
+        .attr("cx", d => tScale(d.date))
+        .attr("cy", d => salesScale(d.sales))
+        .attr("r", 5)
+        .attr("fill", "gold")
 
     svg.selectAll("text")
-        .data(dataset)
+        .data(dateDataset)
         .enter()
         .append("text")
-        .text(d => d)
-        .attr("x", (d, i) => i * w / dataset.length + (w / dataset.length - barPadding) / 2)
-        .attr("y", d => h - 7)
-        .attr("stroke", "white")
-        .attr("fill", "white")
-        .attr("text-anchor", "middle")
+        .text(d => d.sales + ", " + formatTime(d.date))
+        .attr("x", d => tScale(d.date))
+        .attr("y", d => salesScale(d.sales))
     return (
         <>
             <p>
