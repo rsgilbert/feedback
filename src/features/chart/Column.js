45c3ea604913427f406b99094c92c036afd5c0ed
ssenyonjo <gssenyonjo@gmail.com>
2020-08-19 17:20:47 +0300

    set up axis on column chart. Moving on to ordinal scale

--- src/features/chart/Column.js
+++ src/features/chart/Column.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as d3 from 'd3'
+import './Column.css'
 
 
 export const Column = props => {
@@ -11,24 +12,55 @@ export const Column = props => {
     ]
     const w = 500
     const h = 100
-    const padding = 40
+    const xPadding = 40
+    const yPadding = 40
 
 
     const svg = d3.select("#root")
         .append("svg")
-        .attr("width", w)
-        .attr("height", h)
+        .attr("width", w + xPadding)
+        .attr("height", h + 2 * yPadding)
+
+
+    const xScale = d3.scaleLinear()
+        .domain([0, dataset.length])
+        .range([0, w])
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, d => d[1])])
+        .range([0, h])
+
 
 
     svg.selectAll("rect")
         .data(dataset)
         .enter()
         .append("rect")
-        .attr("x", (d, i) => i * ( w / dataset.length))
-        .attr("y", d => h - d[1] * 4)
-        .attr("width", w / dataset.length - padding)
-        .attr("height", d => d[1] * 4)
+        .attr("x", (d, i) => i * ( w / dataset.length) + xPadding)
+        .attr("y", d => h + yPadding - yScale(d[1]))
+        .attr("width", w / dataset.length - xPadding)
+        .attr("height", d => yScale(d[1]))
 
+
+    // axes
+    const xAxis = d3.axisBottom()
+        .scale(xScale)
+
+    svg.append('g')
+        .attr('class', 'column-axis')
+        .attr('transform', `translate(${xPadding},${h + yPadding})`)
+        .call(xAxis)
+        
+
+    const yAxis = d3.axisLeft()
+        .scale(yScale)
+    
+
+    svg.append('g')
+        .attr('class', 'column-axis')
+        .attr('transform', `translate(${xPadding}, ${yPadding})`)
+        .call(yAxis)
+        
 
     return (
         <div className="chart-column">
