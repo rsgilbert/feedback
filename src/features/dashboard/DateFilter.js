463ef9e7c19b4e43380cd64791a602e4cb0e8655
ssenyonjo <gssenyonjo@gmail.com>
2021-02-23 21:15:41 +0300

    working from linux

--- src/features/dashboard/DateFilter.js
+++ src/features/dashboard/DateFilter.js
@@ -12,7 +12,7 @@ export const DateFilter = () => {
                 type="Date" 
                 date={date} 
                 defaultValue={Date()}
-                value={date} />
+               />
         </>
     )
 }
